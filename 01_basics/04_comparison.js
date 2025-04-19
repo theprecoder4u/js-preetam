5b1fea06e8988147d3426e12939fa3d10105ce50
Preetam Patel <preetampatel620@gmail.com>
2025-04-19 07:16:02 +0000

    c

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+// aise conversion kabhi use ni krna bs....<>
+
+console.log(undefined == 0); // ye har condition me false hi dega
+console.log(undefined > 0); // ye har condition me false hi dega
+console.log(undefined < 0); // ye har condition me false hi dega
+
+// === <-- ye triple equals to data type bhi check krta h
+
+console.log("2" === 2);
