fc1246341ecb8ef180391e711ed0cdc48af7d3ab
Preetam Patel <preetampatel620@gmail.com>
2025-04-19 06:09:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS Code as newer version.
+
+// alert(3+3) // we are using nodejs, not browser or html file.
+
+// Make your code readable
+
+console.log( 3 + 3)
+
+console.log("PREETAM") // code readability should be high.
+
+ let name= "preetam"
+ let age= 19
+ let isLoggedIn = false // or true kuch bhi
+
+ //data types :-
+ //number => 2 to power 53
+ //bigint
+ //string = " "
+ //boolean => true/false
+ //null => standalone value h (temperature wala example)
+ //undefined => abhi value assign ni hui h
+ //symbol => uniqueness ke liye
+
+ // object
+
+ //type of
+ console.log(typeof "preetam");
+ console.log(typeof age);
+ console.log(typeof null); // object
+ console.log(typeof undefined); //undefined
