a1f16ba7401f019e8b0c115d4c7c4096e00ce2f4
Preetam Patel <preetampatel620@gmail.com>
2025-04-19 06:51:57 +0000

    conversion-operation

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,75 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); power me aa jata h
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); ye sab ni krna chahiye kyuki messy code kyu likhna h bhai, btw ye 1 dega
+
+
+// console.log(+""); and btw ye 0 dega upar hamne dekha ki "" empty string 0 value deta h isliye...
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter; //prefix increment operator
+//gamecounter++; //postfix increment operator
+console.log(gameCounter);
+
+
+// yaha run krne pe to koi change ni hoga(koi bhi use kro) but dono h alag alag pre mein usage se pehle value increase hoti h and post mein baad mein...
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
